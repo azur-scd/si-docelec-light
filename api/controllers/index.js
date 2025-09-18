decf285f9df17465be87883d05ba79f32af06edc
Mathieu Saby <mathsabypro@gmail.com>
2025-09-18 13:21:28 +0200

    ne pas commenter l'appel au module horaires

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,5 +1,9 @@
+// attention si on commente les lignes sur les horaires on crée une erreur au lancement
+// dans api/routes/routes.js:92
+// app.route('/api/horaires').get(horairesController.list)
+
 const bu = require("./bu");
-// const horaires = require("./horaires");
+const horaires = require("./horaires");
 const user = require("./user");
 const bdd = require("./bdd");
 const bddSignalement = require("./bddSignalement");
@@ -12,7 +16,7 @@ const disc = require("./disc");
 const bddDiscipline = require("./bddDiscipline");
 const sushiHarvest = require("./sushiHarvest")
 module.exports = {bu,
-//                   horaires,
+                  horaires,
                   user,
                   bdd,
                   bddSignalement,
